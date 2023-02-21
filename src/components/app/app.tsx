fbffbb22e17ba2b10a18c8a3b1c280acc15ca14c
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2023-02-21 13:31:52 +0100

    🚚 Move app files to folder

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,9 @@
-import { useProducts } from "../../infrastructure/hooks/use.products";
 import styles from "./app.module.scss";
 
 function App() {
-    // const { products } = useProducts();
-
-    // console.log(products);
-
     return (
         <div className={styles.app}>
             <h2>All Products</h2>
-            {/* <h2>{products[0].title}</h2> */}
         </div>
     );
 }
